ea76e28373157a0230225948dc37773b0af9a4d3
Naman Parashar <namanparashar484@gmail.com>
2023-03-15 20:15:04 +0530

    setting view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,5 +3,7 @@
 // }
 
 module.exports.home = function(req,res){
- return res.end('<h1>Express is up for codial</h1>');
+ return res.render('home',{
+    title:"HOME"
+ });
 }
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const port = 8000;  // generally sites run on port 80.
 
-// use express router
-app.use('/', require('./routes/index'));  // just like import  
 
 app.listen(port,function(err){
     if(err){
@@ -13,3 +11,9 @@ app.listen(port,function(err){
     console.log(`Server is running on port: ${port}`);
 });
 
+// use express router
+app.use('/', require('./routes/index'));  // just like import  
+
+// setting up view engines
+app.set('view engine','ejs');
+app.set('views','./views')  // we can use path module or we can also givi it an array.
