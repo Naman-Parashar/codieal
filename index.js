3b55fa9a5b442b07927d17be3429332bc0d06689
Naman Parashar <namanparashar484@gmail.com>
2023-03-15 19:05:48 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// step 5 of copy
+const express = require('express');
+const app = express();
+const port = 8000;  // generally sites run on port 80.
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running thr surver: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`);
+});
