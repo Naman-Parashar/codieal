3651841612ff32ecbdf59da4bd4e3425529c4ae8
Naman Parashar <namanparashar484@gmail.com>
2023-05-25 18:37:28 +0530

    remove likes

--- models/comment.js
+++ models/comment.js
@@ -15,13 +15,7 @@ const commentSchema = new mongoose.Schema({
     post:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Post'
-    },
-    likes:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Like'
-        }
-    ]
+    }
 },{
     timestamps: true
 });
--- models/post.js
+++ models/post.js
@@ -21,12 +21,6 @@ const postSchema = new mongoose.Schema({
             type:  mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
-    ],
-    likes:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Like'
-        }
     ]
 },{
     timestamps: true
